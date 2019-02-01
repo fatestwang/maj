5b73002d19f334cd9ac1e7f80e758bce1db84916
fatest <fatestwang@gmail.com>
2019-02-01 21:13:21 +0800

    1

---
+++ MAJAI.js
@@ -0,0 +1,27 @@
+class MAJAI{
+    constructor(){
+    }
+
+    init(enviroment, handCard){
+    }
+
+    /**
+     * 每次操作记录
+     * @param {*} player 玩家 
+     * @param {*} operation 操作：吃碰杠摸胡打
+     * @param {*} mainCard 主要操作牌
+     * @param {*} cardIDList 吃碰杠牌合集，特别的加杠不应该有
+     */
+    change(player, operation, mainCard, cardIDList){
+    }
+
+    /**
+     * 响应操作
+     * 
+     */
+    work(){
+        return cardID;
+    }
+}
+
+export default MAJAI;i
